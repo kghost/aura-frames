787aa24997a7eb21a5ecd0787dde311e92c1e7e2
tartarusnl <tartarusnl@883fe7eb-9dc5-4f22-aa82-68726d18b835>
2010-10-29 22:07:37 +0000

    * First import AuraFrames
    git-svn-id: https://repos.curseforge.com/wow/aura-frames/trunk@3 883fe7eb-9dc5-4f22-aa82-68726d18b835

---
+++ AuraFrames/Libs/LibAura-1.0/SpellCooldowns.lua
@@ -0,0 +1,304 @@
+-----------------------------------------------------------------
+--
+--  File: SpellCooldowns.lua
+--
+--  Author: Alex <Nexiuz> Elderson
+--
+--  Description:
+--
+--
+-----------------------------------------------------------------
+
+
+local LibAura = LibStub("LibAura-1.0");
+
+local Major, Minor = "SpellCooldowns-1.0", 0;
+local Module = LibAura:NewModule(Major, Minor);
+
+if not Module then return; end -- No upgrade needed.
+
+-- Make sure that we dont have old unit/types if we upgrade.
+LibAura:UnregisterModuleSource(Module, nil, nil);
+
+-- Register the test unit/types.
+LibAura:RegisterModuleSource(Module, "player", "SPELLCOOLDOWN");
+LibAura:RegisterModuleSource(Module, "pet", "SPELLCOOLDOWN");
+
+
+-- Import most used functions into the local namespace.
+local tinsert, tremove, tconcat, sort = tinsert, tremove, table.concat, sort;
+local fmt, tostring = string.format, tostring;
+local select, pairs, next, type, unpack = select, pairs, next, type, unpack;
+local loadstring, assert, error = loadstring, assert, error;
+local setmetatable, getmetatable, rawset, rawget = setmetatable, getmetatable, rawset, rawget;
+local GetTime = GetTime;
+
+
+-- Internal db used for storing auras, spellbooks and spell history.
+Module.db = Module.db or {};
+
+-- Number of spells to keep in the history list.
+local SpellMaxHistory = 3;
+
+-- Mapping type for booktypes.
+local BookType = {
+  player  = BOOKTYPE_SPELL,
+  pet     = BOOKTYPE_PET,
+};
+
+
+-----------------------------------------------------------------
+-- Function ActivateSource
+-----------------------------------------------------------------
+function Module:ActivateSource(Unit, Type)
+
+  -- We only support Unit "player" and "pet".
+  if Unit ~= "player" and Unit ~= "pet" then
+    return;
+  end
+  
+  -- We only support Type "SPELLCOOLDOWN".
+  if Type ~= "SPELLCOOLDOWN" then
+    return;
+  end
+  
+  if next(self.db) == nil then
+  
+    LibAura:RegisterEvent("SPELLS_CHANGED", self, self.UpdateAllSpellBooks);
+    LibAura:RegisterEvent("UNIT_SPELLCAST_SUCCEEDED", self, self.SpellCasted);
+    LibAura:RegisterEvent("LIBAURA_UPDATE", self, self.ScanAllSpellCooldowns);
+  
+  end
+  
+  if self.db[Unit] then
+    return;
+  end
+  
+  -- Setup the db for the unit.
+  self.db[Unit] = {
+    Book = {},
+    History = {},
+    Auras = {},
+  };
+  
+  -- Fill unit spellbook.
+  self:UpdateSpellBook(Unit);
+  
+end
+
+
+-----------------------------------------------------------------
+-- Function DeactivateSource
+-----------------------------------------------------------------
+function Module:DeactivateSource(Unit, Type)
+
+  if not self.db[Unit] then
+    return;
+  end
+  
+  -- We only support Type "SPELLCOOLDOWN".
+  if Type ~= "SPELLCOOLDOWN" then
+    return;
+  end
+  
+  for _, Aura in ipairs(self.db[Unit].Auras) do
+    LibAura:FireAuraOld(Aura);
+  end
+  
+  self.db[Unit] = nil;
+  
+  if next(self.db) == nil then
+  
+    LibAura:UnregisterEvent("SPELLS_CHANGED", self, self.UpdateAllSpellBooks);
+    LibAura:UnregisterEvent("UNIT_SPELLCAST_SUCCEEDED", self, self.SpellCasted);
+    LibAura:UnregisterEvent("LIBAURA_UPDATE", self, self.ScanAllSpellCooldowns);
+
+  end
+
+end
+
+-----------------------------------------------------------------
+-- Function GetAuras
+-----------------------------------------------------------------
+function Module:GetAuras(Unit, Type)
+
+  -- We only support Unit "player" and "pet".
+  if Unit ~= "player" and Unit ~= "pet" then
+    return {};
+  end
+  
+  -- We only support Type "SPELLCOOLDOWN".
+  if Type ~= "SPELLCOOLDOWN" then
+    return {};
+  end
+  
+  return self.db[Unit].Auras;
+
+
+end
+
+
+-----------------------------------------------------------------
+-- Function UpdateAllSpellBooks
+-----------------------------------------------------------------
+function Module:UpdateAllSpellBooks()
+
+  for Unit, _ in pairs(self.db) do
+    self:UpdateSpellBook(Unit);
+  end
+
+end
+
+
+-----------------------------------------------------------------
+-- Function UpdateSpellBook
+-----------------------------------------------------------------
+function Module:UpdateSpellBook(Unit)
+
+  for _, Aura in pairs(self.db[Unit].Book) do
+    Aura.Old = true;
+  end
+
+  local i = 1
+  while true do
+
+    local _, SpellId = GetSpellBookItemInfo(i, BookType[Unit]);
+
+    if not SpellId then
+      do break end
+    end
+
+    if not self.db[Unit].Book[SpellId] then
+      self.db[Unit].Book[SpellId] = {
+        Type = "SPELLCOOLDOWN",
+        Count = 0,
+        Classification = "None",
+        Unit = Unit,
+        CasterUnit = Unit,
+        CasterName = UnitName(Unit),
+        Duration = 0,
+        ExpirationTime = 0,
+        IsStealable = false,
+        IsCancelable = false,
+        IsDispellable = false,
+      };
+    end
+
+    local Aura = self.db[Unit].Book[SpellId];
+
+    Aura.Index = i;
+    Aura.Name, _, Aura.Icon = GetSpellInfo(SpellId);
+    Aura.SpellId = SpellId;
+    Aura.Id = Unit.."SPELLCOOLDOWN"..Aura.Name;
+    Aura.Old = nil;
+    
+    i = i + 1
+  end
+
+  for SpellId, Aura in pairs(self.db[Unit].Book) do
+    if Aura.Old == true then
+      Aura.Old = nil;
+      self.db[Unit].Book[SpellId] = nil;
+    end
+  end
+
+end
+
+
+-----------------------------------------------------------------
+-- Function ScanAllSpellCooldowns
+-----------------------------------------------------------------
+function Module:ScanAllSpellCooldowns()
+
+  local CurrentTime = GetTime();
+  
+  for _, db in pairs(self.db) do
+  
+    local i = 1;
+    
+    while db.Auras[i] do
+    
+      if db.Auras[i].ExpirationTime <= CurrentTime then
+      
+        LibAura:FireAuraOld(db.Auras[i]);
+        tremove(db.Auras, i);
+      
+      else
+      
+        i = i + 1;
+      
+      end
+      
+    end
+    
+  end
+
+  for Unit, _ in pairs(self.db) do
+    Module:ScanSpellCooldowns(Unit);
+  end
+
+end
+
+
+-----------------------------------------------------------------
+-- Function ScanSpellCooldowns
+-----------------------------------------------------------------
+function Module:ScanSpellCooldowns(Unit)
+
+  i = 1;
+
+  while true do
+  
+    if i > #self.db[Unit].History then
+      break;
+    end
+    
+    local Start, Duration, Active = GetSpellCooldown(self.db[Unit].History[i]);
+    
+    if Active == 1 and Start > 0 and Duration > 3 then
+    
+    
+      local Aura = self.db[Unit].Book[self.db[Unit].History[i]];
+
+      tremove(self.db[Unit].History, i);
+      
+      -- We can have an nil aura (profession cooldown that are not in the spell books).
+      
+      if Aura then
+      
+        Aura.Duration = Duration;
+        Aura.ExpirationTime = Start + Duration;
+        
+        LibAura:FireAuraNew(Aura);
+        
+        tinsert(self.db[Unit].Auras, Aura);
+      
+      end
+      
+    else
+    
+      i = i + 1;
+    
+    end
+    
+  end
+
+end
+
+
+-----------------------------------------------------------------
+-- Function SpellCasted
+-----------------------------------------------------------------
+function Module:SpellCasted(Unit, _, _, _, SpellId)
+
+  if not self.db[Unit] then
+    return;
+  end
+  
+  tinsert(self.db[Unit].History, SpellId);
+
+  if #self.db[Unit].History > SpellMaxHistory then
+    tremove(self.db[Unit].History, 1);
+  end
+
+end
---
+++ AuraFrames/Modules/BarContainer/Module.lua
@@ -0,0 +1,174 @@
+local AuraFrames = LibStub("AceAddon-3.0"):GetAddon("AuraFrames");
+local Module = AuraFrames:NewModule("BarContainer", AuraFrames.ContainerPrototype);
+local LSM = LibStub("LibSharedMedia-3.0");
+local LBF = LibStub("LibButtonFacade");
+
+-- Import most used functions into the local namespace.
+local tinsert, tremove, tconcat, sort = tinsert, tremove, table.concat, sort;
+local fmt, tostring = string.format, tostring;
+local select, pairs, next, type, unpack = select, pairs, next, type, unpack;
+local loadstring, assert, error = loadstring, assert, error;
+local setmetatable, getmetatable, rawset, rawget = setmetatable, getmetatable, rawset, rawget;
+local GetTime = GetTime;
+
+-- Module settings
+Module.MaxBars = 40;
+Module.BarHeight = 24;
+
+-- Register this module as a container handler.
+AuraFrames.ContainerHandlers["Bars"] = Module;
+
+-- List that contains the function prototypes for container objects.
+Module.Prototype = {};
+
+local StatusBarTextures = {
+  ["Aluminum"]    = [[Interface\Addons\AuraFrames\Textures\Aluminum.tga]],
+  ["Armory"]      = [[Interface\Addons\AuraFrames\Textures\Armory.tga]],
+  ["BantoBar"]    = [[Interface\Addons\AuraFrames\Textures\BantoBar.tga]],
+  ["DarkBottom"]  = [[Interface\Addons\AuraFrames\Textures\Darkbottom.tga]],
+  ["Default"]     = [[Interface\Addons\AuraFrames\Textures\Default.tga]],
+  ["Flat"]        = [[Interface\Addons\AuraFrames\Textures\Flat.tga]],
+  ["Glaze"]       = [[Interface\Addons\AuraFrames\Textures\Glaze.tga]],
+  ["Gloss"]       = [[Interface\Addons\AuraFrames\Textures\Gloss.tga]],
+  ["Graphite"]    = [[Interface\Addons\AuraFrames\Textures\Graphite.tga]],
+  ["Minimalist"]  = [[Interface\Addons\AuraFrames\Textures\Minimalist.tga]],
+  ["Otravi"]      = [[Interface\Addons\AuraFrames\Textures\Otravi.tga]],
+  ["Smooth"]      = [[Interface\Addons\AuraFrames\Textures\Smooth.tga]],
+  ["Smooth v2"]   = [[Interface\Addons\AuraFrames\Textures\Smoothv2.tga]],
+  ["Striped"]     = [[Interface\Addons\AuraFrames\Textures\Striped.tga]],
+};
+
+-----------------------------------------------------------------
+-- Function OnInitialize
+-----------------------------------------------------------------
+function Module:OnInitialize()
+
+  self.LBFGroup = LBF:Group("AuraFrames", "Bar Container");
+
+  for Name, Texture in pairs(StatusBarTextures) do
+  
+    if not LSM:Fetch("statusbar", Name, true) then
+      LSM:Register("statusbar", Name, Texture);
+    end
+  
+  end
+
+end
+
+-----------------------------------------------------------------
+-- Function GetName
+-----------------------------------------------------------------
+function Module:GetName()
+
+  return "Bars";
+
+end
+
+-----------------------------------------------------------------
+-- Function GetDescription
+-----------------------------------------------------------------
+function Module:GetDescription()
+
+  return "A container that use bars to display aura's";
+
+end
+
+-----------------------------------------------------------------
+-- Function GetConfigDefaults
+-----------------------------------------------------------------
+function Module:GetConfigDefaults()
+
+  local ConfigDefaults = {
+
+    Location= {
+      OffsetX = 0,
+      OffsetY = 0,
+      FramePoint = "CENTER",
+      RelativePoint = "CENTER",
+    },
+    
+    Layout = {
+      Scale = 1.0,
+      NumberOfBars = 10,
+      Space = 5,
+      BarWidth = 250,
+      BarMaxTime = 30,
+      Direction = "DOWN",
+      DynamicSize = false,
+      ShowDuration = true,
+      DurationLayout = "FORMAT",
+      ShowCount = true,
+      SortOrder = "Duration",
+      ShowTooltip = true,
+      Clickable = true,
+      TooltipShowPrefix = false,
+      TooltipShowCaster = true,
+      TooltipShowSpellId = false,
+      TooltipShowClassification = false,
+      BarTexture = "Aluminum",
+      BarDirection = "LEFTSHRINK",
+      Icon = "LEFT",
+    },
+
+    Colors = {
+      Debuff = {
+        None        = {0.8, 0.0, 0.0, 1.0},
+        Magic       = {0.2, 0.6, 1.0, 1.0},
+        Curse       = {0.6, 0.0, 1.0, 1.0},
+        Disease     = {0.6, 0.4, 0.0, 1.0},
+        Poison      = {0.0, 0.6, 0.0, 1.0},
+      },
+      Buff          = {1.0, 1.0, 1.0, 0.0},
+      Weapon        = {1.0, 1.0, 1.0, 0.0},
+      Other         = {1.0, 1.0, 1.0, 0.0},
+    },
+    
+    Order = {},
+    Filter = {},
+  };
+  
+  return ConfigDefaults;
+
+end
+
+
+-----------------------------------------------------------------
+-- Function New
+-----------------------------------------------------------------
+function Module:New(Config)
+
+  local Container = {};
+  setmetatable(Container, { __index = self.Prototype});
+  
+  -- Reuse old containers if we can.
+  if _G["AuraFramesBarContainer_"..Config.Name] then
+    Container.Frame = _G["AuraFramesBarContainer_"..Config.Name];
+  else
+    Container.Frame = CreateFrame("Frame", "AuraFramesBarContainer_"..Config.Name, UIParent, "AuraFramesBarContainerTemplate");
+  end
+  
+  Container.Frame:Show();
+
+  Container.Name = Config.Name;
+  Container.ConfigMode = false;
+  Container.Config = Config;
+  
+  Container.Filter = AuraFrames:NewFilter(Config.Filter, function() Container:Update("FILTER"); end);
+  
+  Container.Order = AuraFrames:NewOrder(Config.Order, function() Container:Update("ORDER"); end);
+  
+  Container.TooltipOptions = {};
+  
+  Container.Bars = {};
+  
+  Container:Update();
+  
+  Container.Frame:SetScript("OnEvent", function() Container:Update(); end);
+  Container.Frame:RegisterEvent("PLAYER_ENTERING_WORLD");
+  Container.Frame:RegisterEvent("ZONE_CHANGED");
+
+  Container:SetConfigMode(AuraFrames.ConfigMode);
+
+  return Container;
+
+end
