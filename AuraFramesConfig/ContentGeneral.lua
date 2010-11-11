a9050f9a60bd1fbf9222deadbb8e005740b101f3
tartarusnl <tartarusnl@883fe7eb-9dc5-4f22-aa82-68726d18b835>
2010-11-11 16:40:47 +0000

    Changed the version display a little bit
    git-svn-id: https://repos.curseforge.com/wow/aura-frames/trunk@147 883fe7eb-9dc5-4f22-aa82-68726d18b835

--- AuraFramesConfig/ContentGeneral.lua
+++ AuraFramesConfig/ContentGeneral.lua
@@ -40,7 +40,7 @@ function AuraFramesConfig:ContentGeneral()
   VersionGroup:AddSpace();
   
   VersionGroup:AddText("Version", nil, 100);
-  VersionGroup:AddText("|cffff0000: "..AuraFrames.Version.String.."|r", nil, 450);
+  VersionGroup:AddText(": |cffff0000"..AuraFrames.Version.String.."|r", nil, 450);
   
   VersionGroup:AddText("Revision", nil, 100);
   VersionGroup:AddText(": "..AuraFrames.Version.Revision, nil, 450);
@@ -51,6 +51,6 @@ function AuraFramesConfig:ContentGeneral()
   self.Content:AddSpace();
   self.Content:AddHeader("Credits");
 
-  self.Content:AddText("This addon is developed and mainted by |cff9382C9Nexiuz|r (|cff0070DDBeautiuz|r) @ Bloodhoof EU.\n\nSome code are based on other addons, the two most imported addons that helped me and inspired me are SatrinaBuffFrame and LibBuffet.\n\nSpecial thanks goes to |cff9382C9Ripsomeone|r @ Bloodhoof EU for testing and helping me giving the addon his current form.");
+  self.Content:AddText("This addon is developed and mainted by |cff9382C9Nexiuz|r (|cff0070DDBeautiuz|r) @ Bloodhoof EU.\n\nThe two most important addons that helped me and inspired me are SatrinaBuffFrame and LibBuffet.\n\nSpecial thanks goes to |cff9382C9Ripsomeone|r @ Bloodhoof EU for testing and helping me giving the addon his current form.");
 
 end
